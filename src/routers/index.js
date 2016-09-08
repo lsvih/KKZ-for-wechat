f11155839a6fa4bf2cf15446a31fbbcb86eefd71
lsvih <408772145@qq.com>
2016-09-08 12:23:27 +0800

    Finish footer of index,init router

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,10 +1,27 @@
 import HomeView from '../views/home/HomeView.vue';
+import User from '../views/user/User.vue';
+import Login from '../views/user/Login.vue';
+import Caculate from '../views/custom/Caculate.vue';
+import Appointment from '../views/custom/Appointment.vue';
 import NotFound from '../views/404.vue';
+
 
 export default (router) => {
     router.map({
         '/': {
             component: HomeView
+        },
+        '/user': {
+            component: User
+        },
+        '/login': {
+            component: Login
+        },
+        '/caculate': {
+            component: Caculate
+        },
+        '/appointment': {
+            component: Appointment
         },
         '/404': {
             component: NotFound
--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -1,21 +1,35 @@
 <template>
 <footer>
-<div class="user"></div>
-<div class="caculate"></div>
-<div class="appointment">立即预约</div>
+<div class="user" @click="_user">
+  <div class="icon"></div>
+  <span v-if="!user">登录/注册</span>
+  <span v-if="user">{{user}}</span>
+</div>
+<div class="caculate" @click="_caculate">
+  <div class="icon"></div>
+  <span>计算</span></div>
+<div class="appointment" @click="_appointment">立即预约</div>
 </footer>
 </template>
 
 <script>
 import "../../scss/homeView.scss"
 export default {
-    components: {
-
-    },
     data() {
         return {
-
+          user:localStorage.user
         };
     },
+    methods:{
+      _user:function(){
+        this.$route.router.go(this.user?{path:"/user"}:{path:"/login"})
+      },
+      _caculate:function(){
+        this.$route.router.go({path:"/caculate"});
+      },
+      _appointment:function(){
+        this.user?this.$route.router.go({path:"/appointment"}):this._user();
+      }
+    }
 }
 </script>
